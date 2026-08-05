37cfc86531bcb7660fd396f709ed0f6e4915e516
VercosaV <victorhvercosag@gmail.com>
2026-08-05 17:52:06 -0300

    sw

--- script.js
+++ script.js
@@ -760,7 +760,7 @@ function renderNotes() {
   grid.style.display = "grid";
   grid.innerHTML = "";
 
-  const noteColorsList =  ;
+  const noteColorsList = null;
 
   notesToShow.forEach(note => {
     const card = document.createElement("div");
