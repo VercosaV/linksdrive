92bdbaf087113e2ba78bce87aab79c5450456d33
VercosaV <victorhvercosag@gmail.com>
2026-08-06 08:47:26 -0300

    array no colorlist

--- script.js
+++ script.js
@@ -722,7 +722,7 @@ function renderNotes() {
   if (!grid || !empty) return;
 
   // Declarado no início para evitar o erro de inicialização (TDZ)
-const noteColorsList = Array.from({ length: 6 }, (_, i) => i + 1);
+  const noteColorsList = [1, 2, 3, 4, 5, 6];
 
   const folderStrip = document.getElementById("folderStrip");
   if (folderStrip) {
